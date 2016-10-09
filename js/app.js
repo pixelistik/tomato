df55bf7e3522257d315b3f5e36805fb84a94554c
Pixelistik <code@pixelistik.de>
2016-10-09 17:54:31 +0200

    Simple version, counting seconds

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-var vue = require("vue");
+"use strict";
+
+var Vue = require("vue");
+
+window.vm = new Vue({
+    el: "#app",
+    data: {
+        title: "One",
+        workStartedTime: false,
+        pauseStartedTime: false,
+        now: new Date(),
+        timer: false
+    },
+    created: function () {
+        this.timer = setInterval(this.updateTimes, 1000);
+    },
+    methods: {
+        startWorking: function () {
+            console.debug("startng to work");
+            this.workStartedTime = new Date();
+        },
+        updateTimes: function () {
+            console.debug("Updating");
+            this.now = new Date();
+        }
+    },
+    computed: {
+        workingTime: function () {
+            return Math.round((this.now - this.workStartedTime) / 1000);
+        }
+    }
+
+});
